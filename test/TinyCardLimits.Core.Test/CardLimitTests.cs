a70624bbdf080f2f9bc73279b12ed82f65822fb5
kparginos <kparginos@yahoo.gr>
2021-02-23 11:59:05 +0200

    Add tests for Card and CardLimit services

---
+++ test/TinyCardLimits.Core.Test/CardLimitTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using TinyCardLimits.Core.Consts;
+using TinyCardLimits.Core.Model.Types;
+using TinyCardLimits.Core.Services.Interfaces;
+using TinyCardLimits.Core.Services.Options;
+
+using Xunit;
+
+
+namespace TinyCardLimits.Core.Test
+{
+    public class CardLimitTests : IClassFixture<TinyCardLimitsFixture>
+    {
+        private ICardLimitService _cardLimit;
+
+        public CardLimitTests(TinyCardLimitsFixture fixture)
+        {
+            _cardLimit = fixture.Scope.ServiceProvider
+                .GetRequiredService<ICardLimitService>();
+        }
+
+        #region Test Async methods
+        [Fact]
+        public async void Add_CardLimit_Success_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "9876489674127531",
+                TransactionType = TransactionTypes.CardPresent,
+                TransAmount = 400.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        [Fact]
+        public async void Add_CardLimit_Success_UpdateExistingLimits_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "9876411114127531",
+                TransactionType = TransactionTypes.eCommerce,
+                TransAmount = 400.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        [Fact]
+        public async void Add_CardLimit_Fail_UpdateExistingLimits_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "9876411114127531",
+                TransactionType = TransactionTypes.eCommerce,
+                TransAmount = 400.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        [Fact]
+        public async void Add_CardLimit_Fail_UpdateExistingPRESENTLimits_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "9876411114127531",
+                TransactionType = TransactionTypes.CardPresent,
+                TransAmount = 1000.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        [Fact]
+        public async void Add_CardLimit_Fail_Empty_CardNum_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "",
+                TransactionType = TransactionTypes.CardPresent,
+                TransAmount = 400.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        [Fact]
+        public async void Add_CardLimit_Fail_Amount_Exceed_CardPresent_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "9876489674127531",
+                TransactionType = TransactionTypes.CardPresent,
+                TransAmount = 400.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        [Fact]
+        public async void Add_CardLimit_Fail_Amount_Exceed_eCommerce_Async()
+        {
+            var options = new RegisterCardLimitOptions()
+            {
+                CardNumber = "9876489674127531",
+                TransactionType = TransactionTypes.CardPresent,
+                TransAmount = 400.0m
+            };
+
+            var result = await _cardLimit.RegisterAsync(options);
+
+            Assert.Equal(ResultCodes.Success, result.Code);
+        }
+        #endregion Test Async methods
+    }
+}
---
+++ test/TinyCardLimits.Core.Test/TinyCardLimitsFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using TinyCardLimits.Core.Services.Extentions;
+
+namespace TinyCardLimits.Core.Test
+{
+    public class TinyCardLimitsFixture : IDisposable
+    {
+        public IServiceScope Scope { get; private set; }
+
+        public TinyCardLimitsFixture()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath($"{AppDomain.CurrentDomain.BaseDirectory}")
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Initialize Depedency Conttainer
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddAppServices(config);
+
+            Scope = serviceCollection
+                .BuildServiceProvider()
+                .CreateScope();
+        }
+
+        public void Dispose()
+        {
+            Scope.Dispose();
+        }
+    }
+}
